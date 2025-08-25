c43245689c34e6f2160c8221733c3c59443a656a
XoDeR <xoder.vs@gmail.com>
2025-08-25 22:51:37 +0200

    backend auth: auth routes; added middleware for CRUD routes

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('articles', ArticleController::class, [
         'only' => ['index', 'store', 'update', 'destroy']
     ]);
