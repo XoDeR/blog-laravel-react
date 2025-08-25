fa9939cd0739aaa628654c34a5aa1684ef16eb58
XoDeR <xoder.vs@gmail.com>
2025-08-25 22:22:22 +0200

    All articles are displayed as cards with additional action buttons

--- blog-client/src/components/ArticleCard.tsx
+++ blog-client/src/components/ArticleCard.tsx
@@ -1,10 +1,41 @@
-const ArticleCard = () => {
+import type { Article } from "@/types";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+
+interface Props {
+  article: Article,
+}
+
+const ArticleCard = ({article}: Props) => {
+  const handleDelete = (articleId: number) => {
+  }
 
   return (
-    <div>
-      Card
-    </div>
-  )
+    <Card className="h-full overflow-hidden hover:shadow-md transition-shadow">
+      <CardHeader>
+          <CardTitle className="text-xl">{article.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm text-muted-foreground">{article.content}</p>
+      </CardContent>
+      <CardFooter className="mt-auto">
+        <Button 
+          variant="secondary" 
+          size="sm"
+          onClick={() => {}}
+        >
+          Edit
+        </Button>
+        <Button 
+          variant="destructive" 
+          size="sm"
+          onClick={() => handleDelete(article.id)}
+        >
+          Delete
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default ArticleCard;
--- blog-client/src/components/ArticleList.tsx
+++ blog-client/src/components/ArticleList.tsx
@@ -1,8 +1,21 @@
+import { useArticles } from "@/hooks/useArticles";
+import ArticleCard from "./ArticleCard";
+
 const ArticleList = () => {
 
-  return (
-    <div>
-      List
+const { data: articles, isLoading, isError } = useArticles();
+
+if (isLoading) return <div>Loading articles...</div>
+
+if (isError) return <div>Error fetching articles.</div>
+
+return (
+    <div className="mx-auto max-w-6xl p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        {articles?.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
     </div>
   )
 }
