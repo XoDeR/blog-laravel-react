4f407520ba34ec812a7f4a29fe19b9983d485d77
XoDeR <xoder.vs@gmail.com>
2025-08-25 22:54:32 +0200

    frontend auth: token is added to headers if it's present

--- blog-client/src/api/api.ts
+++ blog-client/src/api/api.ts
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: "http://localhost:8000/api/v1",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("blog-token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
